f1b8e01f2e8b304a60cd402505d27d115e3c7ab1
ECassaroNumen <eric.cassaro@numenit.com>
2025-06-16 14:23:15 -0300

    feat: configs iniciais do TS

---
+++ srv/main.js
@@ -0,0 +1,4 @@
+function f1(p1) {
+    console.log("Function f1 called: p1=" + p1);
+}
+f1("test");
---
+++ srv/main.ts
@@ -0,0 +1,4 @@
+function f1(p1: string) {
+  console.log("Function f1 called: p1=" + p1);
+}
+f1("test");
