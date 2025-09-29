fd36636f25849a476eb3bdf5d00cd60e051bbe16
ECassaroNumen <eric.cassaro@numenit.com>
2025-09-29 17:41:15 -0300

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
 import cds, { Request, Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import { Customers, Products, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service)=>{
     service.after('READ', 'Customers', (results: Customers)=>{
@@ -8,6 +8,7 @@ export default (service: Service)=>{
 
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request)=>{
         const params = request.data
+        const items: SalesOrderItems = params.items;
 
         if (!params.items || params.items?.length === 0) {
             request.reject(400, 'At least one item is required');
@@ -23,6 +24,22 @@ export default (service: Service)=>{
 
         if (!customer) {
             request.reject(404, `Customer with id ${params.customer_id} not found`);
+        }
+
+        //validar os produtos dos items
+        const productIds: String[] = params.items.map((item: SalesOrderItem)=> item.product_id);
+        const productQuery = SELECT.from('sales.Products').where( { id: productIds } );
+        const products: Products = await cds.run(productQuery);
+        const dbProducts = products.map( (product) => product.id );
+
+        for (const item of items) {
+            const dbProduct = products.find( (product) => product.id === item.product_id );            
+            if (!dbProduct) {
+                request.reject(404, `Product with id ${item.product_id} not found`);
+            }
+            if (dbProduct?.stock === 0) {
+                request.reject(400, `Product ${dbProduct.name} is out of stock`);
+            }
         }
     });
 }
