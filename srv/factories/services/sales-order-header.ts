e2e974c079103735cb5d6564f498cc067823cffd
ECassaroNumen <eric.cassaro@numenit.com>
2026-01-22 16:10:48 -0300

    feat: add new dependency for SalesOrderHeader factory

--- srv/factories/services/sales-order-header.ts
+++ srv/factories/services/sales-order-header.ts
@@ -2,11 +2,13 @@ import { CustomerRepositoryImpl } from "srv/repositories/customer/implementatin"
 import { ProductRepositoryImpl } from "srv/repositories/product/implementation";
 import { SalesOrderHeaderServiceImpl } from "srv/services/sales-order-header/implementation";
 import { SalesOrderHeaderService } from "srv/services/sales-order-header/protocol";
+import { SalesOrderLogRepositoryImpl } from "srv/repositories/sales-order-log/implementation";
 
 const makeSalesOrderHeaderService = ():SalesOrderHeaderService => {
     const customerRepository = new CustomerRepositoryImpl();
     const productRepository =  new ProductRepositoryImpl();
-    return new SalesOrderHeaderServiceImpl(customerRepository,productRepository);
+    const salesOrderLogRepository = new SalesOrderLogRepositoryImpl();
+    return new SalesOrderHeaderServiceImpl(customerRepository,productRepository,salesOrderLogRepository);
 }
 
 export const salesOrderHeaderService = makeSalesOrderHeaderService();
